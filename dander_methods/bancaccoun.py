036f4cf83af8bc9c790a9426862f99b199c8de63
Beknazar Abdyldaev <abdyldaev.duff@gmail.com>
2022-12-08 22:13:24 +0600

    add logic __add__ two obj

--- dander_methods/bancaccoun.py
+++ dander_methods/bancaccoun.py
@@ -4,10 +4,15 @@ class BancAccount:
         self.balance = balance
     
     def __add__(self, other):
+        if isinstance(other, BancAccount):
+            return self.balance + other.balance
+        
         if isinstance(other, (int, float)):
             return self.balance + other
 
 
 Beka = BancAccount("Beka", 1000)
 
-print(Beka + 100)
+Nurs = BancAccount("Nurs", 1000)
+
+print(Beka + Nurs)
